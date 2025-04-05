1345f338ac2665b319722af21fb209abb67d6e88
Esha Patel <Patellesha11@gmail.com>
2025-04-05 12:15:00 +0300

    added mode

--- src/Components/Toggle.js
+++ src/Components/Toggle.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import './Toggle.css'
 
 function Toggle () {
+    const [mode, setMode] = useState("false")
     return (
         <div>
             <h1>GOOGLE</h1>
